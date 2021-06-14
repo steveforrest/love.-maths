7a2d96c94d5b390b3f14f5ef8ed7059896776bab
steveforrest <steveforrest_1@hotmail.com>
2021-06-14 11:06:41 +0000

    added funtion names to script

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkanswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractionQuestion(){
+
+}
+
+function displayMultiplicationQuestion(){
+
+}
+
+function displayDivisionQuestion(){
+
+}
