4bf218916e8cbb209143cb53185efed1f4104ca5
steveforrest <steveforrest_1@hotmail.com>
2021-06-14 17:41:05 +0000

    Add code to display addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,27 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
                 } else {
                     let gameType = this.getAttribute("data-type");
-                    alert(`youclicked ${gameType}`);
+                    runGame(gameType);
                 }
         })
     }
+    runGame("addition");
 })
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
- function runGame(){
-    let num1 = Math.ceiling(Math.random()*25);
+ function runGame(gameType) {
+
+    let num1 = Math.ceil(Math.random()*25);
     let num2 = Math.floor(Math.random() * 25 +1); 
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`unknown gametype: ${gameType}`)
+        throw `unknown gametype: ${gameType}. Aborting!`;
+    }
 }
 
 function checkanswer(){
@@ -42,7 +50,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
